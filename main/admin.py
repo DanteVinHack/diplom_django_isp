9b589caf4c1eca86dad77e3dea66c4b4bf6b403e
DanteVinHack <ventilyatortag@gmail.com>
2026-02-17 12:41:01 +0300

    add template path and remove defualt comments

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Position, Passport, Worker
-# Register your models here.
+
 @admin.register(Position)
 class PositionAdmin(admin.ModelAdmin):
     list_display = ["name"]
